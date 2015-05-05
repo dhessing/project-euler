a94e8e741ea26a3e58a98c727e145346ae073443
dhessing <dhessing@gmail.com>
2015-05-05 21:17:03 +0200

    Adds problem descriptions

--- src/project_euler/problem_1.clj
+++ src/project_euler/problem_1.clj
@@ -1,5 +1,12 @@
 (ns project-euler.problem-1)
 
+;;;; Problem 1
+
+;;; If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+;;; get 3, 5, 6 and 9. The sum of these multiples is 23.
+;;;
+;;; Find the sum of all the multiples of 3 or 5 below 1000.
+
 (defn problem-1 [limit]
   (apply + (set (concat (range 3 limit 3) (range 5 limit 5)))))
 
