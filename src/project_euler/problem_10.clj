65494b76b74394df622784a109109006ca1dd394
dhessing <dhessing@gmail.com>
2015-05-11 12:43:49 +0200

    Solves problem 10 using problem 7
    Finding the answer is a bit on the slow side though

---
+++ src/project_euler/problem_10.clj
@@ -0,0 +1,15 @@
+;;;; Summation of primes
+
+;;; The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+;;; Find the sum of all the primes below two million.
+
+(ns project-euler.problem-10
+  (:require project-euler.problem-7))
+
+(defn sum-of-primes [n]
+  (->> (project-euler.problem-7/prime-seq)
+       (take-while (partial > n))
+       (apply +)))
+
+(defn -main [] (sum-of-primes 2000000))
