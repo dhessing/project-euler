8880665dcc1c1f2ef16732c81893740ee29f6a03
dhessing <dhessing@gmail.com>
2015-06-04 08:06:34 +0200

    Solves problem 17

---
+++ src/project_euler/problem_17.clj
@@ -0,0 +1,10 @@
+(ns project-euler.problem-17)
+
+(defn number-letter-counts [n]
+  (let [length (count (re-seq #"\w" (clojure.pprint/cl-format nil "~R" n)))]
+    (if (and (> n 100) (not= 0 (rem n 100)))
+      (+ 3 length)                                          ; Compensates for 'and'
+      length)))
+
+(defn -main []
+  (println (apply + (map number-letter-counts (range 1 1001)))))
