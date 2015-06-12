620cb28afc64c746b0ec9f3dd2c87302eb7efb90
dhessing <dhessing@gmail.com>
2015-06-12 08:34:22 +0200

    Solves problem 18

---
+++ test/project_euler/problem_18_test.clj
@@ -0,0 +1,6 @@
+(ns project-euler.problem-18-test
+  (:require [clojure.test :refer :all])
+  (:require [project-euler.problem-18 :refer :all]))
+
+(deftest test-add-rows
+  (is (= (add-rows [2 3] [1]) [4])))
