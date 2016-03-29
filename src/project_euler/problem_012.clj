4d255d319a6eafcfbe1cdfd702b74594840e45e8
dhessing <dhessing@gmail.com>
2016-03-29 22:27:11 +0200

    Updates problem 12

--- src/project_euler/problem_012.clj
+++ src/project_euler/problem_012.clj
@@ -1,18 +1,59 @@
 ;;;; Highly divisible triangular number
-;;; https://projecteuler.net/problem=12
 
-(ns project-euler.problem-012)
+(ns project-euler.problem-012
+  (:require [clojure.math.numeric-tower :as math])
+  (:use clojure.test))
 
 (defn triangle-numbers []
-  (map first (iterate (fn [[x y]] [(+ x y) (inc y)]) [1 2])))
+  (reductions + (iterate inc 1)))
 
-(defn number-of-divisors [x]
-  (->> (range 1 (Math/sqrt x))
-       (filter #(zero? (mod x %)))
-       (count)
-       (* 2)))
+(defn prime? [n]
+  (.isProbablePrime (BigInteger/valueOf n) 10))
 
-(defn -main []
-  (->> (triangle-numbers)
-       (filter #(< 500 (number-of-divisors %)))
-       (first)))
+(defn prime-seq []
+  (filter prime? (range)))
+
+(defn lowest-prime-divisor [x primes]
+  (letfn [(divides? [x p] (zero? (mod x p)))]
+    (->> (take-while (partial >= x) primes)
+         (filter (partial divides? x))
+         (first))))
+
+(defn prime-factors [x primes]
+  (lazy-seq
+    (when-let [prime-factor (lowest-prime-divisor x primes)]
+      (cons prime-factor (prime-factors (/ x prime-factor) primes)))))
+
+(defn number-of-divisors [x primes]
+  (->> (prime-factors x primes)
+       (frequencies)
+       (vals)
+       (map inc)
+       (apply *)))
+
+(deftest test-prime?
+  (is (true? (prime? 7))))
+
+(deftest test-triangle-numbers
+  (is (= '(1 3 6 10 15 21 28) (take 7 (triangle-numbers)))))
+
+(deftest test-prime-seq
+  (is (= '(2 3 5 7 11 13 17) (take 7 (prime-seq)))))
+
+(deftest test-lowest-prime-divisor
+  (is (= 2 (lowest-prime-divisor 28 (prime-seq)))))
+
+(deftest test-lowest-prime-divisor-is-prime
+  (is (= 7 (lowest-prime-divisor 7 (prime-seq)))))
+
+(deftest test-prime-factors
+  (is (= '(2 2 7) (prime-factors 28 (prime-seq)))))
+
+(deftest test-number-of-divisors
+  (is (= 6 (number-of-divisors 28 (prime-seq)))))
+
+(defn solve []
+  (let [primes (prime-seq)]
+    (->> (triangle-numbers)
+         (filter #(< 500 (number-of-divisors % primes)))
+         (first))))
