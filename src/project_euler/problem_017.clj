a9e60fd2e1d08801f2ee1bfd877e8521792d2d39
dhessing <dhessing@gmail.com>
2016-04-05 08:50:43 +0200

    Updates problem 17

--- src/project_euler/problem_017.clj
+++ src/project_euler/problem_017.clj
@@ -1,10 +1,29 @@
-(ns project-euler.problem-017)
+(ns project-euler.problem-017
+  (:use clojure.test))
+
+(defn number->english [n]
+  (let [english (clojure.pprint/cl-format nil "~R" n)
+        words (clojure.string/split english #"\s")]
+    (if (< 2 (count words))
+      (let [[w1 w2 w3] words]
+        (clojure.string/join " " [w1 w2 "and" w3]))
+      english)))
 
 (defn number-letter-counts [n]
-  (let [length (count (re-seq #"\w" (clojure.pprint/cl-format nil "~R" n)))]
-    (if (and (> n 100) (not= 0 (rem n 100)))
-      (+ 3 length)                                          ; Compensates for 'and'
-      length)))
+  (count (re-seq #"\w" (number->english n))))
 
-(defn -main []
-  (println (apply + (map number-letter-counts (range 1 1001)))))
+(deftest test-number-to-english
+  (is (= (number->english 342) "three hundred and forty-two"))
+  (is (= (number->english 115) "one hundred and fifteen"))
+  (is (= (number->english 200) "two hundred")))
+
+(deftest test-number-letter-counts
+  (is (= (number-letter-counts 1) 3))
+  (is (= (number-letter-counts 342) 23))
+  (is (= (number-letter-counts 115) 20)))
+
+(deftest example
+  (is (= 19 (apply + (map number-letter-counts (range 1 6))))))
+
+(defn solve []
+  (apply + (map number-letter-counts (range 1 1001))))
--- test/project_euler/problem_17_test.clj
+++
@@ -1,9 +0,0 @@
-(ns project-euler.problem-17-test
-  (:require [clojure.test :refer :all])
-  (:require [project-euler.problem-017 :refer :all]))
-
-(deftest test-number-letter-counts
-  (is (= (number-letter-counts 1) 3))
-  (is (= (number-letter-counts 342) 23))
-  (is (= (number-letter-counts 115) 20))
-  (is (= (number-letter-counts 200) 10)))
