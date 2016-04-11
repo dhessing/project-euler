755121685518a87622c885de0469a245c5afb731
dhessing <dhessing@gmail.com>
2016-04-11 15:13:25 +0200

    solves problem 20

---
+++ src/project_euler/problem_020.clj
@@ -0,0 +1,18 @@
+(ns project-euler.problem-020
+  (:require [project-euler.problem-008 :as problem-008])
+  (:use clojure.test))
+
+(defn factorial [n]
+  (reduce *' (range 1 (inc n))))
+
+(defn factorial-digit-sum [n]
+  (apply + (map int (problem-008/to-digits (factorial n)))))
+
+(deftest test-factorial
+  (is (= 3628800 (factorial 10))))
+
+(deftest test-factorial-digit-sum
+  (is (= (factorial-digit-sum 10) 27)))
+
+(defn solve []
+  (factorial-digit-sum 100))
