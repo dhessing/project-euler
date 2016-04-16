d0589f8dd85a11717dd42997258814b5bcb2948d
dhessing <dhessing@gmail.com>
2016-04-16 20:41:05 +0200

    Cleans up problem 23

--- src/project_euler/problem_023.clj
+++ src/project_euler/problem_023.clj
@@ -5,25 +5,19 @@
 (defn abundant-number? [n]
   (< n (reduce + (p21/proper-divisors n))))
 
-(defn abundant-numbers [n]
+(defn abundant-numbers-seq [n]
   (filter abundant-number? (range 1 (inc n))))
 
-(defn- sum-of-two-abundant-numbers? [abundant-numbers abundant-number-set n]
-  (some
-    #(contains? abundant-number-set %)
-    (for [x abundant-numbers
-          :while (<= (/ n 2))]
-      (- n x))))
+(defn sum-of-two-abundant-numbers?' [abundant-numbers abundant-number-set n]
+  (some #(contains? abundant-number-set %) (for [x abundant-numbers] (- n x))))
 
 (deftest test-sum-of-two-abundant-numbers?
-  (let [abundant-numbers (abundant-numbers 28124)
+  (let [abundant-numbers (abundant-numbers-seq 28124)
         abundant-number-set (set abundant-numbers)]
-    (is (true? (sum-of-two-abundant-numbers? abundant-numbers abundant-number-set 24)))
-    (is (true? (sum-of-two-abundant-numbers? abundant-numbers abundant-number-set 28124)))))
+    (is (true? (sum-of-two-abundant-numbers?' abundant-numbers abundant-number-set 24)))
+    (is (true? (sum-of-two-abundant-numbers?' abundant-numbers abundant-number-set 28124)))))
 
 (defn solve []
-  (let [abundant-numbers (abundant-numbers 28124)
-        sum-of-two-abundant-numbers? (partial sum-of-two-abundant-numbers? abundant-numbers (set abundant-numbers))]
-    (->> (range 1 28124)
-         (remove sum-of-two-abundant-numbers?)
-         (reduce +))))
+  (let [abundant-numbers (abundant-numbers-seq 28124)
+        sum-of-two-abundant-numbers? (partial sum-of-two-abundant-numbers?' abundant-numbers (set abundant-numbers))]
+    (reduce + (remove sum-of-two-abundant-numbers? (range 1 28124)))))
