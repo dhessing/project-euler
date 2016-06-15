fe1cafe4f8ebea5f72b4702e69a698717c7a4ac6
dhessing <dhessing@gmail.com>
2016-06-15 13:58:55 +0200

    Updates problem 30

--- src/project_euler/level_02/problem_030.clj
+++ src/project_euler/level_02/problem_030.clj
@@ -1,12 +1,11 @@
 (ns project-euler.level-02.problem-030
-  (:require [project-euler.level-01.problem-008 :as problem-008]
-            [clojure.math.numeric-tower :as math])
+  (:require [clojure.math.numeric-tower :as math])
   (:use clojure.test))
 
 (defn n-power-digits [n]
   (let [numbers (range 10 (inc (* n (math/expt 9 n))))]
     (for [number numbers
-          :let [digits (problem-008/to-digits number)
+          :let [digits (map #(Integer. (str %)) (str number))
                 sum (reduce + (map #(math/expt % n) digits))]
           :when (= sum number)]
       number)))
