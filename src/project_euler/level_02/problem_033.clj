b73de2a664e9194a7f6fc3088b240ab11b49b6dd
dhessing <dhessing@gmail.com>
2016-06-24 16:59:35 +0200

    Solves problem 33

---
+++ src/project_euler/level_02/problem_033.clj
@@ -0,0 +1,25 @@
+(ns project-euler.level-02.problem-033
+  (:require [clojure.math.combinatorics :as combo]))
+
+(defn int-to-digits [n]
+  (map #(Integer/parseInt (str %)) (str n)))
+
+(defn digits-to-int [coll]
+  (Integer/parseInt (clojure.string/join (map str coll))))
+
+
+(denominator
+  (reduce *
+    (for [[a b] (combo/combinations (range 10 100) 2)
+          :let [digits-a (set (int-to-digits a))
+                digits-b (set (int-to-digits b))
+                digits-a-cancelled (clojure.set/difference digits-a digits-b)
+                digits-b-cancelled (clojure.set/difference digits-b digits-a)]
+          :when (not-any? empty? [digits-a-cancelled digits-b-cancelled])
+          :let [c (digits-to-int digits-a-cancelled)
+                d (digits-to-int digits-b-cancelled)]
+          :when (try (and (not= (rem a 10) 0)
+                          (not= digits-a digits-a-cancelled)
+                          (= (/ a b) (/ c d)))
+                     (catch Exception e false))]
+      (/ a b))))
