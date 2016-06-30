6ac469dd8a817cd32149b659b6e2d1e6bf1e2d87
dhessing <dhessing@gmail.com>
2016-06-30 15:08:26 +0200

    Updates problem 33

--- src/project_euler/level_02/problem_033.clj
+++ src/project_euler/level_02/problem_033.clj
@@ -8,18 +8,20 @@
   (Integer/parseInt (clojure.string/join (map str coll))))
 
 
-(denominator
-  (reduce *
-    (for [[a b] (combo/combinations (range 10 100) 2)
-          :let [digits-a (set (int-to-digits a))
-                digits-b (set (int-to-digits b))
-                digits-a-cancelled (clojure.set/difference digits-a digits-b)
-                digits-b-cancelled (clojure.set/difference digits-b digits-a)]
-          :when (not-any? empty? [digits-a-cancelled digits-b-cancelled])
-          :let [c (digits-to-int digits-a-cancelled)
-                d (digits-to-int digits-b-cancelled)]
-          :when (try (and (not= (rem a 10) 0)
-                          (not= digits-a digits-a-cancelled)
-                          (= (/ a b) (/ c d)))
-                     (catch Exception e false))]
-      (/ a b))))
+(defn digit-cancelling-fractions []
+  (for [[a b] (combo/combinations (range 10 100) 2)
+        :let [digits-a (set (int-to-digits a))
+              digits-b (set (int-to-digits b))
+              digits-a-cancelled (clojure.set/difference digits-a digits-b)
+              digits-b-cancelled (clojure.set/difference digits-b digits-a)]
+        :when (not-any? empty? [digits-a-cancelled digits-b-cancelled])
+        :let [c (digits-to-int digits-a-cancelled)
+              d (digits-to-int digits-b-cancelled)]
+        :when (try (and (not= (rem a 10) 0)
+                        (not= digits-a digits-a-cancelled)
+                        (= (/ a b) (/ c d)))
+                   (catch Exception _ false))]
+    (/ a b)))
+
+(defn solve []
+  (denominator (reduce * (digit-cancelling-fractions))))
