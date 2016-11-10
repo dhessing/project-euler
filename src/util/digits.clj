62ab98a3dedd33145787b2dee98067357798fee4
dhessing <dhessing@gmail.com>
2016-11-10 19:02:25 +0100

    Extracted primes and used digits for problem 35

--- src/util/digits.clj
+++ src/util/digits.clj
@@ -1,7 +1,14 @@
-(ns util.digits)
+(ns util.digits
+  (:use [clojure.test]))
 
 (defn from-int [n]
   (map #(Integer/parseInt (str %)) (str n)))
 
+(deftest test-from-int
+  (is (= (from-int 123) [1 2 3])))
+
 (defn to-int [coll]
   (Integer/parseInt (clojure.string/join (map str coll))))
+
+(deftest test-to-int
+  (is (= (to-int [1 2 3]) 123)))
