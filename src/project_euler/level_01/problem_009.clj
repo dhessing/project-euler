949cda6c020c139be467e43ed5051ae55dd665df
Dzjon Hessing <dhessing@gmail.com>
2019-04-18 14:42:04 +0200

    Deletes everything.

--- src/project_euler/level_01/problem_009.clj
+++
@@ -1,6 +0,0 @@
-;;;; Special Pythagorean triplet
-
-(ns project-euler.level-01.problem-009
-  (:require [lib.pythagoras :as pythagoras]))
-
-(defn solve [] (apply * (first (pythagoras/triplets 1000))))
--- src/project_euler/level_01/problem_015.clj
+++
@@ -1,5 +0,0 @@
-(ns project-euler.level-01.problem-015
-  (:import (cern.jet.math.tdouble DoubleArithmetic)))
-
-(defn solve []
-  (long (DoubleArithmetic/binomial 40 20)))
